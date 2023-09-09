e7925b7ce56666a9162ff3bbe44355fe1ef80f6d
Stefan <stefan@stefanpuia.co.uk>
2023-09-09 18:27:14 +0100

    db field sizes, bumped transport

--- src/util/ResultReportUnitSanitiser.ts
+++ src/util/ResultReportUnitSanitiser.ts
@@ -5,35 +5,41 @@ import { ResultReport } from "../model/Report";
 export class ResultReportUnitSanitiser {
   public sanitise(unit: ResultReport, baseURL: string): ResultReport {
     return {
-      ...this.sanitiseField("title", unit.title),
-      ...this.sanitiseField("description", unit.description),
+      ...this.sanitiseField("title", 250, unit.title),
+      ...this.sanitiseField("description", 500, unit.description),
       ...this.sanitiseURL(baseURL, "image", unit.image),
       ...this.sanitiseURL(baseURL, "url", unit.url)
     };
   }
 
-  private sanitiseField(fieldName: keyof ResultReport, fieldValue?: string): ResultReport | undefined {
+  private sanitiseField(fieldName: keyof ResultReport, maxLength: number, fieldValue?: string): ResultReport | undefined {
     return fieldValue
       ? {
-          [fieldName]: this.sanitiseValue(fieldValue)
+          [fieldName]: this.sanitiseValue(fieldValue).substring(0, maxLength)
         }
       : undefined;
   }
 
-  private makeURL(baseURL: string, path: string): string {
-    const url = new URL(path, baseURL);
+  private makeURL(baseURL: string, pathRaw: string): string {
+    const path = this.sanitiseValue(pathRaw);
+    let url;
+    try {
+      url = new URL(path);
+    } catch (_) {
+      url = new URL(path, baseURL);
+    }
     return url.href;
   }
 
   private sanitiseURL(baseURL: string, fieldName: keyof ResultReport, fieldValue?: string): ResultReport | undefined {
     return fieldValue
       ? {
-          [fieldName]: this.makeURL(baseURL, this.sanitiseValue(fieldValue))
+          [fieldName]: this.makeURL(baseURL, this.sanitiseValue(fieldValue)).substring(0, 2048)
         }
       : undefined;
   }
 
   private sanitiseValue(value: string) {
-    return value.trim().substring(0, 255);
+    return value.trim();
   }
 }
